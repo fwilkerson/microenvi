2f5a9be979585e4c800c14836bacb962f11e9c44
Frank Wilkerson <FWilkerson@ad.exetered.com>
2018-01-23 21:52:32 -0700

    concurrently setup

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import {h, render} from 'preact';
 
 import './lib/socket';
 
-render(<h2>All the micro</h2>, document.querySelector('#root'));
+render(<h2>All the micro!</h2>, document.querySelector('#root'));
