3168fcb52c553b4434ad8840541b30ea6089ff92
Frank Wilkerson <FWilkerson@ad.exetered.com>
2018-01-24 22:36:04 -0700

    eliminate concurrently by running the bundler and server in the same process

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ const url = require('url');
 const chokidar = require('chokidar');
 const {send} = require('micro');
 const compress = require('micro-compress');
+const microbundle = require('microbundle');
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({port: 3301});
 const watcher = chokidar.watch(path.join(__dirname, 'public'));
+
+wss.on('connection', ws => {
+	ws.on('error', error => {
+		// most likely this means the user manually refreshed the browser
+	});
+});
 
 watcher.on('change', (path, stats) => {
 	// a better solution would be to debeounce the reload event
@@ -19,6 +26,14 @@ watcher.on('change', (path, stats) => {
 		if (client.readyState === WebSocket.OPEN) client.send('reload');
 	});
 });
+
+// I'll need to get this info from the user
+microbundle({
+	cwd: __dirname,
+	format: 'es',
+	jsx: 'h',
+	watch: true,
+}).catch(console.error);
 
 function serve(request, response) {
 	const {pathname} = url.parse(request.url);
