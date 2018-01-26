76d1df03d6d356836fd7e05b64d92c9fd65e66c5
Frank Wilkerson <FWilkerson@ad.exetered.com>
2018-01-25 23:11:56 -0700

    update demos

---
+++ demo/muve-quickstart/src/index.js
@@ -0,0 +1,13 @@
+import muve, {h, interact} from 'muve';
+
+// MODEL
+const init = {};
+
+// UPDATE
+const {getModel, setModel} = interact(init);
+
+// VIEW
+const view = model => <h2>Hello, Microenvi</h2>;
+
+// RENDER
+muve(view, init, document.getElementById('root'));
