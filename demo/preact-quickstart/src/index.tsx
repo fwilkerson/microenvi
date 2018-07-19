b8f1ae4afd18314c271f9db2954b37c979a6e27b
Frank A. Wilkerson <frank.wilkerson@outlook.com>
2018-07-18 18:58:24 -0700

    files for tsx

---
+++ demo/preact-quickstart/src/index.tsx
@@ -0,0 +1,33 @@
+import {Component, h, render} from 'preact';
+
+const styles = {
+	center: {textAlign: 'center'},
+	spacing: {margin: '0 1em'},
+};
+
+class App extends Component<{}, {count: number}> {
+	constructor(props) {
+		super(props);
+		this.state = {count: 0};
+	}
+
+	updateCount(value) {
+		return e => {
+			this.setState({count: this.state.count + value});
+		};
+	}
+
+	render(props, state) {
+		return (
+			<main style={styles.center}>
+				<h2>A fun micro dev environment!</h2>
+
+				<button onClick={this.updateCount(-1)}>−</button>
+				<em style={styles.spacing}>{state.count}</em>
+				<button onClick={this.updateCount(1)}>+</button>
+			</main>
+		);
+	}
+}
+
+render(<App />, document.querySelector('#root'));
